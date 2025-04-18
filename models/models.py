e685443f00da765f24d6b564cd86b7d7080698b7
markunya <mabondarenko223@gmail.com>
2025-04-18 20:04:01 +0300

    identity

--- models/models.py
+++ models/models.py
@@ -1,4 +1,10 @@
+from torch import nn
 from utils.class_registry import ClassRegistry
 
 LRELU_SLOPE = 0.1
 models_registry = ClassRegistry()
+
+@models_registry.add_to_registry(name='identity')
+class IdentityModel(nn.Module):
+    def forward(self, wav, *args, **kwargs):
+        return wav.clone()
